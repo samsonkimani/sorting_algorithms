448f3a991a0ccce48babc081475558c3d48e8484
samsonkimani <samsonkimani43@gmail.com>
2023-01-20 18:54:30 +0300

    created insertion sort and modified bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,11 +21,10 @@ void bubble_sort(int *array, size_t size)
 			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
-
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 	}
 }
