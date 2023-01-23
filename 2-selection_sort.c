ad3588a63ef46616189f10b4a48dab796fea3efd
samsonkimani <samsonkimani43@gmail.com>
2023-01-23 13:10:21 +0300

    making the selection sort more efficient

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,25 +10,40 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, position;
+	size_t i, minindex, maxindex, left = 0, right = size - 1;
 	int swap;
 
 	if (array == NULL)
 		return;
-	for (i = 0; i < size - 1; i++)
+	while (left < right)
 	{
-		position = i;
-		for (j = i + 1; j < size; j++)
+		minindex = left;
+		maxindex = right;
+		for (i = left; i <= right; i++)
 		{
-			if (array[j] < array[position])
-				position = j;
+			if (array[i] < array[minindex])
+				minindex = i;
+			if (array[i] > array[maxindex])
+				maxindex = i;
 		}
-		if (position != i)
+		if (minindex != left)
 		{
-			swap = array[i];
-			array[i] = array[position];
-			array[position] = swap;
-			print_array(array, size);
+			swap = array[left];
+			array[left] = array[minindex];
+			array[minindex] = swap;
 		}
+
+		if (maxindex == left)
+			maxindex = minindex;
+
+		if (maxindex != right)
+		{
+			swap = array[right];
+			array[right] = array[maxindex];
+			array[maxindex] = swap;
+		}
+		print_array(array, size);
+		left++;
+		right--;
 	}
 }
