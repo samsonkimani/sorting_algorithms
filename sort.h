64b6268718a5a4b26ab9e3e6449b988199e640c9
samsonkimani <samsonkimani43@gmail.com>
2023-01-23 11:17:13 +0300

    correcting betty errors in header file

--- sort.h
+++ sort.h
@@ -11,11 +11,12 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
